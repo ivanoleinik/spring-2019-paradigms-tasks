dca1df7c4cdc1e02a1b1d6a9feb71e74a3f99a02
ivanoleinik <olivva239@gmail.com>
2019-04-05 18:33:57 +0300

    Add PrettyPrinter class, pretty_print function and test for them

---
+++ task06/printer.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import model
+
+
+class PrettyPrinter(model.ASTNodeVisitor):
+    def __init__(self):
+        self.enable_indent = 1
+        self.indent_depth = 0
+
+    def pretty_print(self, program):
+        self.enable_indent = 1
+        self.indent_depth = 0
+        return f'{program.accept(self)};'
+
+    def indent(self):
+        return '    ' * (self.indent_depth * self.enable_indent)
+
+    def visit_number(self, number):
+        return self.indent() + str(number.value)
+
+    def visit_function(self, function):
+        raise RuntimeError('PrettyPrinter must not visit function object')
+
+    def visit_function_definition(self, function_definition):
+        result = '{}def {}({}) {{\n'.format(
+            self.indent(),
+            function_definition.name,
+            ', '.join(function_definition.function.args)
+        )
+        self.indent_depth += 1
+        for statement in function_definition.function.body or []:
+            result += f'{statement.accept(self)};\n'
+        self.indent_depth -= 1
+        result += '}'
+        return result
+
+    def visit_conditional(self, conditional):
+        self.enable_indent -= 1
+        condition_result = conditional.condition.accept(self)
+        self.enable_indent += 1
+        result = f'{self.indent()}if ({condition_result}) {{\n'
+
+        self.indent_depth += 1
+        for statement in conditional.if_true or []:
+            result += f'{statement.accept(self)};\n'
+        self.indent_depth -= 1
+        result += self.indent() + '}'
+
+        if conditional.if_false:
+            result += ' else {\n'
+            self.indent_depth += 1
+            for statement in conditional.if_false:
+                result += f'{statement.accept(self)};\n'
+            self.indent_depth -= 1
+            result += self.indent() + '}'
+        return result
+
+    def visit_print(self, print_object):
+        self.enable_indent -= 1
+        result = print_object.expr.accept(self)
+        self.enable_indent += 1
+        return f'{self.indent()}print {result}'
+
+    def visit_read(self, read_object):
+        return f'{self.indent()}read {read_object.name}'
+
+    def visit_function_call(self, function_call):
+        self.enable_indent -= 1
+        result = []
+        for arg in function_call.args:
+            result.append(arg.accept(self))
+        self.enable_indent += 1
+        return '{}({})'.format(
+            function_call.fun_expr.accept(self),
+            ', '.join(result)
+        )
+
+    def visit_reference(self, reference):
+        return self.indent() + reference.name
+
+    def visit_binary_operation(self, binary_operation):
+        self.enable_indent -= 1
+        lhs_result = binary_operation.lhs.accept(self)
+        rhs_result = binary_operation.rhs.accept(self)
+        self.enable_indent += 1
+        return self.indent() + \
+            f'({lhs_result}) {binary_operation.op} ({rhs_result})'
+
+    def visit_unary_operation(self, unary_operation):
+        self.enable_indent -= 1
+        result = unary_operation.expr.accept(self)
+        self.enable_indent += 1
+        return f'{self.indent()}{unary_operation.op}({result})'
+
+
+def pretty_print(program):
+    print(PrettyPrinter().pretty_print(program))
---
+++ task06/test_printer.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+import pytest
+
+import model
+import printer
+
+
+def test_pretty_number(capsys):
+    printer.pretty_print(model.Number(239))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == '239;\n'
+
+
+def test_pretty_function_definition(capsys):
+    printer.pretty_print(
+        model.FunctionDefinition("foo", model.Function([], []))
+    )
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == 'def foo() {\n};\n'
+
+
+def test_pretty_conditional(capsys):
+    printer.pretty_print(model.Conditional(model.Number(239), [], []))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == 'if (239) {\n};\n'
+
+
+def test_pretty_print(capsys):
+    printer.pretty_print(model.Print(model.Number(239)))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == 'print 239;\n'
+
+
+def test_pretty_read(capsys):
+    printer.pretty_print(model.Read('var'))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == 'read var;\n'
+
+
+def test_pretty_function_call(capsys):
+    printer.pretty_print(
+        model.FunctionCall(
+            model.Reference('foo'),
+            [
+                model.Number(2),
+                model.Number(3),
+                model.Number(9)
+            ]
+        )
+    )
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == 'foo(2, 3, 9);\n'
+
+
+def test_pretty_reference(capsys):
+    printer.pretty_print(model.Reference('var'))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == 'var;\n'
+
+
+def test_pretty_binary_operation(capsys):
+    mul = model.BinaryOperation(model.Number(1), '*', model.Number(2))
+    add = model.BinaryOperation(model.Number(3), '+', mul)
+    mod = model.BinaryOperation(mul, '%', add)
+    printer.pretty_print(mod)
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == '((1) * (2)) % ((3) + ((1) * (2)));\n'
+
+
+def test_pretty_unary_operation(capsys):
+    printer.pretty_print(model.UnaryOperation('-', model.Number(239)))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == '-(239);\n'
+
+
+def test_pretty_factorial(capsys):
+    yat_fac = model.FunctionDefinition('fac', model.Function(['n'], [
+        model.Conditional(
+            model.BinaryOperation(model.Reference('n'), '==', model.Number(0)),
+            [model.Number(1)],
+            [
+                model.BinaryOperation(
+                    model.Reference('n'),
+                    '*',
+                    model.FunctionCall(model.Reference('fac'), [
+                        model.BinaryOperation(
+                            model.Reference('n'),
+                            '-',
+                            model.Number(1)
+                        )
+                    ])
+                )
+            ]
+        )
+    ]))
+    printer.pretty_print(yat_fac)
+    out, err = capsys.readouterr()
+    assert not err
+    assert out.split('\n') == [
+        'def fac(n) {',
+        '    if ((n) == (0)) {',
+        '        1;',
+        '    } else {',
+        '        (n) * (fac((n) - (1)));',
+        '    };',
+        '};',
+        ''
+    ]
+
+
+if __name__ == "__main__":
+    pytest.main()
